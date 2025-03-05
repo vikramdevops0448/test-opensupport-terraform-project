d822d61dc15fb5df4d54247114d1aaa5ffea5a5d
vikramdevops0448 <vikramdevops0448@gmail.com>
2025-03-06 00:03:28 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -9,3 +9,12 @@ resource "aws_instance" "app_instance" {
     Name = "${var.environment}-app-instance"
   }
 }
+
+terraform {
+  backend "s3" {
+    bucket = var.backend_bucket
+    key    = var.backend_key
+    region = var.backend_region
+  }
+}
+
