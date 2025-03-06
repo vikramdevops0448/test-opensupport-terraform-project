0147cd5d685387e6ebe7a4ce424e5f22eb1603b8
vikramdevops0448 <vikramdevops0448@gmail.com>
2025-03-06 21:37:09 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app_instance" {
   ami                         = "ami-05b10e08d247fb927"  # Replace with a valid AMI
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public_subnet.id
-  security_groups             = [aws_security_group.ec2_security_group.name]
+  security_groups             = aws_security_group.ec2_security_group.id
   key_name                    = "jenkins"  # Add your SSH key
 
   tags = {
